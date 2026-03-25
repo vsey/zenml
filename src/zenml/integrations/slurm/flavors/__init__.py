621cd4d5b674c887a50954c869a54821ad1320c4
vsey <usokk@student.kit.edu>
2026-03-25 17:22:06 +0100

    add flavor init

---
+++ src/zenml/integrations/slurm/flavors/__init__.py
@@ -0,0 +1,11 @@
+from zenml.integrations.slurm.flavors.Slurm_step_operator_flavor import (
+    SlurmStepOperatorConfig,
+    SlurmStepOperatorFlavor,
+    SlurmStepOperatorSettings,
+)
+
+__all__ = [
+    "SlurmStepOperatorConfig",
+    "SlurmStepOperatorFlavor",
+    "SlurmStepOperatorSettings",
+]
